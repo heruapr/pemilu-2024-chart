21cd67c49084b1d529bfc02bf4f2748eadb3bb02
heruapr <heruapr@gmail.com>
2024-02-15 16:42:03 +0700

    add column view on desktop

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
-import ChartDataLabels from 'chartjs-plugin-datalabels';
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 Chart.register(ChartDataLabels);
 
@@ -38,9 +38,9 @@ export default function Home() {
               label: data.label,
               data: data.data,
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
               ],
               borderColor: data.borderColor,
               borderWidth: 1,
@@ -52,9 +52,9 @@ export default function Home() {
           plugins: {
             legend: {
               position: "top",
-            //   datalabels: {
-            //     color: '#36A2EB'
-            //   }
+              //   datalabels: {
+              //     color: '#36A2EB'
+              //   }
             },
             title: {
               display: true,
@@ -84,7 +84,7 @@ export default function Home() {
 
           item.value.forEach((val, index) => {
             if (val.percentage !== undefined) {
-              chartItem.labels.push('PASLON '+(index+1));
+              chartItem.labels.push("PASLON " + (index + 1));
               chartItem.data.push(val.percentage);
               chartItem.backgroundColor.push(getRandomColor());
               chartItem.borderColor.push(getRandomColor());
@@ -113,20 +113,39 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <h1>Election Pie Chart: The data is retrieved by web scraping from www.kawalpemilu.org.</h1>
-      <h2>Please note that this data is not final; refer to <a href="https://kawalpemilu.org">www.kawalpemilu.org</a> for detailed information. </h2>
-      <h4>maintained by heruapr</h4>
-      {/* <h3>Contact heruapr.dev@gmail.com if you need the web API for this web-scraped data.</h3> */}
-      {scrapedData ? (
-        scrapedData.map((_, index) => (
-          <div key={index}>
-            <canvas ref={(ref) => (canvasRefs.current[index] = ref)}></canvas>
-          </div>
-        ))
-      ) : (
-        <button onClick={fetchData}>Fetch Data</button>
-      )}
+    // <div>
+    //   <h1>Election Pie Chart: The data is retrieved by web scraping from www.kawalpemilu.org.</h1>
+    //   <h2>Please note that this data is not final; refer to <a href="https://kawalpemilu.org">www.kawalpemilu.org</a> for detailed information. </h2>
+    //   <h4>maintained by heruapr</h4>
+    //   {/* <h3>Contact heruapr.dev@gmail.com if you need the web API for this web-scraped data.</h3> */}
+    //   {scrapedData ? (
+    //     scrapedData.map((_, index) => (
+    //       <div key={index}>
+    //         <canvas ref={(ref) => (canvasRefs.current[index] = ref)}></canvas>
+    //       </div>
+    //     ))
+    //   ) : (
+    //     <button onClick={fetchData}>Fetch Data</button>
+    //   )}
+    // </div>
+<div>
+  <h1>Election Pie Chart: The data is retrieved by web scraping from www.kawalpemilu.org.</h1>
+  <h2>Please note that this data is not final; refer to <a href="https://kawalpemilu.org">www.kawalpemilu.org</a> for detailed information. </h2>
+  <h4>maintained by heruapr</h4>
+  {scrapedData ? (
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gridGap: '10px' }}>
+      {scrapedData.map((_, index) => (
+        <div key={index}>
+          <canvas ref={(ref) => (canvasRefs.current[index] = ref)}></canvas>
+        </div>
+      ))}
     </div>
+  ) : (
+    <button onClick={fetchData}>Fetch Data</button>
+  )}
+</div>
+
+
+
   );
 }
